0386236eb2ac7899967380801dc9185b77ef6991
aquiiino <danilo.aquiino@outlook.com>
2025-09-29 01:13:23 -0300

    javascript da consultorlogin, mesma coisa da clientelogin, recebe o form e redireciona

--- js/clienteLogin.js
+++ js/clienteLogin.js
@@ -1,5 +1,6 @@
 // aguarda o envio do formulário e redireciona para index.html se os campos obrigatórios estiverem preenchidos
-document.getElementById("formLogin").addEventListener("submit", function(event) {
+document.getElementById("formLoginCliente").addEventListener("submit", function(event) {
     event.preventDefault(); 
     window.location.href = "index.html"; 
 });
+

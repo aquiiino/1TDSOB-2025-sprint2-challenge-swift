ff1d92f3db336d7ff7e6d2dcc5a813652526eedd
ithalloelzo <ithalloelzo@gmail.com>
2025-09-30 20:10:15 -0300

    clienteLogin.js, index.js, Cliente_Login.html e index.html

--- js/clienteLogin.js
+++ js/clienteLogin.js
@@ -4,3 +4,22 @@ document.getElementById("formLoginCliente").addEventListener("submit", function(
     window.location.href = "index.html"; 
 });
 
+//pega o nome do cliente e guarda
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('formLoginCliente');
+
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        const nome = document.getElementById('nomeCliente').value;
+        const senha = form.senhaCliente.value;
+
+        // Simulação simples do login
+        if (nome && senha) {
+            localStorage.setItem('clienteLogado', nome); // salva o nome
+            window.location.href = '../pages/index.html';       // redireciona
+        } else {
+            alert('Preencha todos os campos!');
+        }
+    });
+});
---
+++ js/index.js
@@ -0,0 +1,11 @@
+//exibi a mensagem na pagina de index
+document.addEventListener('DOMContentLoaded', () => {
+    const nomeCliente = localStorage.getItem('clienteLogado');
+    const mensagem = document.getElementById('mensagemCliente');
+
+    if (nomeCliente) {
+        mensagem.textContent = `Olá, ${nomeCliente}!`;
+    } else {
+        window.location.href = 'Cliente_Login.html'; // redireciona se não logado
+    }
+});

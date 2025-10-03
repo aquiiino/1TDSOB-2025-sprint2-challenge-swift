6c32f99c2f9525bd8c473fc4aaf685aaab5fcf96
ithalloelzo <ithalloelzo@gmail.com>
2025-10-02 21:46:45 -0300

    consultor indo para sua index.html

--- js/consultoraLogin.js
+++ js/consultoraLogin.js
@@ -1,5 +1,5 @@
 // aguarda o envio do formulário e redireciona para index.html se os campos obrigatórios estiverem preenchidos, mesma coisa do outro js
 document.getElementById("formLoginConsultor").addEventListener("submit", function(event) {
     event.preventDefault(); 
-    window.location.href = "index.html"; 
+    window.location.href = "index_consultor.html"; 
 });
